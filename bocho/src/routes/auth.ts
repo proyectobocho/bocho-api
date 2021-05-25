6fc32381ac2b9f6bee277737bb0507f89e8223db
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2021-05-25 15:02:18 -0300

    prueba

--- bocho/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import AuthController from "../controller/AuthController";
-
-const router =Router();
-
-//login
-router.post('/login',AuthController.login);
-
-export default router;
